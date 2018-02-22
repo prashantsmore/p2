0fc8c594b1d8eca24e0d3eaf8ef9170d7d28756d
Prashant More <prashantsmore@gmail.com>
2018-02-21 22:04:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,11 +17,11 @@ require 'validation-logic.php';
 </head>
 <body>
 <div class="container">
-    <DIV class='h1info'  align='center' >Bill Splitter</DIV>
+    <DIV class='h1info' >Bill Splitter</DIV>
 
     <form name="bill-form" method='GET' action='index.php'>
 
-        <DIV class="h1info">Accepts The Total Amount Spent & Adds Tip Percentage And Calculates Individual Share.</DIV>
+        <H2 class="h1info" >Accepts The Total Amount Spent & Adds Tip Percentage And Calculates Individual Share.</H2>
 
         <div  class="info">
             <label for='split'>Split How Many Ways? (required|numeric|min 1|max 4)</label>
